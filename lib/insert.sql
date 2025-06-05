d62c1fbce373caec4b5bab071824dfd165c77f82
jared <jaredrotich3@gmail.com>
2025-06-05 17:59:06 +0300

    finalised and succesfully fixed the test

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 12, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 8, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 5, 'F', 'White', 'Gentle', 1),
+('Sergeant Brown', 10, 'M', 'Brown', 'Tough', 1),
+('Melissa', 7, 'F', 'Grey', 'Playful', 1),
+('Grinch', 9, 'M', 'Green', 'Grumpy', 0),
+('Wendy', 4, 'F', 'Black', 'Shy', 1),
+(NULL, 6, 'M', 'Cinnamon', 'Curious', 1);
